0996e8ed00760a8f7568e4f105d48c2e1c70e8d0
oninotna <antoninomusarratubbi@gmail.com>
2025-04-15 15:44:26 +0200

    added card in html of present object and started form code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
+// #####################################
+// FUNCTION
+function addCardMemberTeam (team, section) {
+  for (member of team) {
+    const {name, role, email, img} = member;
+  
+    section.innerHTML += 
+    `<div class="col-sm-12 col-md-6 col-lg-4">
+      <div class="card mb-3">
+        <div class="row g-0">
+          <div class="col-4">
+            <img class="img-fluid rounded-start" src="${img}" alt="${name}">
+          </div>
+          <div class="col-8">
+            <div class="card-body vertical-align-middle">
+              <h5 class="card-title mt-1">${name}</h5>
+              <p class="card-text mb-1">${role}</p>
+              <p class="card-text"><small class="text-body-secondary">${email}</small></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`
+  };
+};
+
+// ##########################################
+// INIZIO CODICE
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -36,3 +64,10 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+// RACCOLTA ELEMENTI DOM
+const cardTeamSection = document.getElementById("card-team-section");
+
+
+// INVOCAZIONE FUNZIONE PER AGGIUNGERE CARD
+addCardMemberTeam(teamMembers, cardTeamSection);
